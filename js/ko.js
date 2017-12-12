a9faeebb68ed62b6d67de2a0675cc8798b2693a1
Laura Logan <laurardh@aol.com>
2017-12-12 12:02:26 -0500

    adds list to app with KO adds input bar to filter list

--- js/ko.js
+++ js/ko.js
@@ -2,32 +2,134 @@
 // location data
 //POI locations
 var locations = [
-  {title: 'Monte Cassino', location: {lat: 41.4903315,lng: 13.8133865}, id: 'ChIJfY4t5_C7OhMRUtdE4FkRmlI'},
-  {title: 'Abbazia di Casamari', location: {lat: 41.6712011,lng: 13.4871188}, id: 'ChIJPYa6mI1TJRMRTgnwCfQC3J8'},
-  {title: 'Grotte di Pastena', location: {lat: 41.4965448,lng: 13.490696}, id: 'ChIJZ9xeWKJLJRMRZnyvq7OPiJM'},
-  {title: 'Certosa di Trisulti', location: {lat: 41.77965289999999,lng: 13.3972988}, id: 'ChIJDb1wNuNXJRMRLq0HXCRrRSg'},
-  {title: 'Santuario di Vallepietra', location: {lat: 41.9563957,lng: 13.2311503}, id: 'ChIJFYB8yZXtLxMR_3n6HBquE5o'},
-  {title: 'Monte Cacume', location: {lat: 41.5666667,lng: 13.2333333}, id: 'ChIJNXQpv8dDJRMRIcft4b0u5jQ'},
-  {title: 'Grotte di Collepardo', location: {lat: 41.7583057,lng: 13.3647073}, id: 'ChIJ0yhRnfBXJRMR8SaXPuElASs'},
-  {title: 'Tempio di Juve', location: {lat: 41.2907741,lng: 13.2601077}, id: 'ChIJw4O8OlwjJRMRcDghq5tPCQ8'},
-  {title: "Terracina", location: {lat: 41.2841265,lng: 13.2467785}, id: 'ChIJmWqgT2wjJRMRk6s2_dnYr_M'},
-  {title: 'Capo Fiume', location: {lat: 41.781229,lng: 13.4105518}, id: 'ChIJBYR_YIv4LxMRid62zl2jRXY'},
-  {title: 'Dal Patricano', location: {lat: 41.574439,lng: 13.269065}, id: 'ChIJbf8ZVolEJRMRPE2CqSzE9qQ'},
-  {title: 'Valle dei Lepini', location: {lat: 41.5929749,lng: 13.2830475}, id: 'ChIJfSRiRZJEJRMRTnPO6LgMURo'},
-  {title: 'La Torretta', location: {lat: 41.593569,lng: 13.251473}, id: 'ChIJCXCjrnFDJRMRvMvZUcrQ4Ls'},
-  {title: 'Creazioni Dolciarie Zambardino', location: {lat: 41.5741811,lng: 13.3233213}, id: 'ChIJA2gDt6lFJRMRXr9Uy5JVT1k'},
-  {title: 'La Mola', location: {lat: 41.6405164,lng: 13.2077839}, id: 'ChIJJX8p7DBDJRMR6lJdWg8DuCc'},
-  {title: 'La Scifa', location: {lat: 41.6459897,lng: 13.3519947}, id: 'ChIJu9FnaoRaJRMR0A_48vJtmbg'},
-  {title: 'La Cometa', location: {lat: 41.647915,lng: 13.3367828}, id: 'ChIJV6wowYxaJRMRSKuQdISk6Ac'},
-  {title: 'Pizzeria Brunella', location: {lat: 41.6520719,lng: 13.3412584}, id: 'ChIJIQYtJ4paJRMRFq83AgFu9Pc'},
-  {title: 'Le Migliori Pizza alla Pala', location: {lat: 41.611247,lng: 13.228897}, id: 'ChIJL-IzCCNDJRMR2yyzEnXAF04'},
+  {title: 'Monte Cassino',
+  location: {lat: 41.4903315,lng: 13.8133865},
+  id: 'ChIJfY4t5_C7OhMRUtdE4FkRmlI'
+  },
+  {title: 'Abbazia di Casamari',
+  location: {lat: 41.6712011,lng: 13.4871188},
+  id: 'ChIJPYa6mI1TJRMRTgnwCfQC3J8'
+  },
+  {title: 'Grotte di Pastena',
+  location: {lat: 41.4965448,lng: 13.490696},
+  id: 'ChIJZ9xeWKJLJRMRZnyvq7OPiJM'
+  },
+  {title: 'Certosa di Trisulti',
+  location: {lat: 41.77965289999999,
+  lng: 13.3972988},
+  id: 'ChIJDb1wNuNXJRMRLq0HXCRrRSg'
+  },
+  {title: 'Santuario di Vallepietra',
+  location: {lat: 41.9563957,lng: 13.2311503},
+  id: 'ChIJFYB8yZXtLxMR_3n6HBquE5o'
+  },
+  {title: 'Monte Cacume',
+  location: {lat: 41.5666667,lng: 13.2333333},
+  id: 'ChIJNXQpv8dDJRMRIcft4b0u5jQ'
+  },
+  {title: 'Grotte di Collepardo',
+  location: {lat: 41.7583057,lng: 13.3647073},
+  id: 'ChIJ0yhRnfBXJRMR8SaXPuElASs'
+  },
+  {title: 'Tempio di Juve',
+  location: {lat: 41.2907741,lng: 13.2601077},
+  id: 'ChIJw4O8OlwjJRMRcDghq5tPCQ8'
+  },
+  {title: "Terracina",
+  location: {lat: 41.2841265,lng: 13.2467785},
+  id: 'ChIJmWqgT2wjJRMRk6s2_dnYr_M'
+  },
+  {title: 'Capo Fiume',
+  location: {lat: 41.781229,lng: 13.4105518},
+  id: 'ChIJBYR_YIv4LxMRid62zl2jRXY'
+  },
+  {title: 'Dal Patricano',
+  location: {lat: 41.574439,lng: 13.269065},
+  id: 'ChIJbf8ZVolEJRMRPE2CqSzE9qQ'
+  },
+  {title: 'Valle dei Lepini',
+  location: {lat: 41.5929749,lng: 13.2830475},
+  id: 'ChIJfSRiRZJEJRMRTnPO6LgMURo'
+  },
+  {title: 'La Torretta',
+  location: {lat: 41.593569,lng: 13.251473},
+  id: 'ChIJCXCjrnFDJRMRvMvZUcrQ4Ls'
+  },
+  {title: 'Creazioni Dolciarie Zambardino',
+  location: {lat: 41.5741811,lng: 13.3233213},
+  id: 'ChIJA2gDt6lFJRMRXr9Uy5JVT1k'
+  },
+  {title: 'La Mola', location:
+  {lat: 41.6405164,lng: 13.2077839},
+  id: 'ChIJJX8p7DBDJRMR6lJdWg8DuCc'
+  },
+  {title: 'La Scifa',
+  location: {lat: 41.6459897,lng: 13.3519947},
+  id: 'ChIJu9FnaoRaJRMR0A_48vJtmbg'
+  },
+  {title: 'La Cometa',
+  location: {lat: 41.647915,lng: 13.3367828},
+  id: 'ChIJV6wowYxaJRMRSKuQdISk6Ac'
+  },
+  {title: 'Pizzeria Brunella',
+  location: {lat: 41.6520719,lng: 13.3412584},
+  id: 'ChIJIQYtJ4paJRMRFq83AgFu9Pc'
+  },
+  {title: 'Le Migliori Pizza alla Pala',
+  location: {lat: 41.611247,lng: 13.228897},
+  id: 'ChIJL-IzCCNDJRMR2yyzEnXAF04'
+  },
   // {title: 'La Terrazza', location: {lat: ,lng:}, id: },
-  {title: 'La Passeggiata', location: {lat: 41.5916799,lng: 13.2427548}, id: 'ChIJpR7JqQxDJRMRzk_oQbpBuLA'},
-  {title: 'Ricordi Lievitati', location: {lat: 41.590483,lng: 13.241865}, id: 'ChIJ1dX5PwtDJRMRYeOfHiynQ_k'},
-  {title:  'Abbazia di Fossanova', location: {lat: 41.4381536,lng: 13.1957528}, id: 'ChIJw8cFRMo_JRMRujicGH4ep0E'},
-  {title:  'Villa d\'Este', location: {lat: 41.9633123,lng: 12.7958058}, id: 'ChIJw-YlX3B4LxMRmeEzTuqQHr8'},
-  {title:  'Reggia Caserta', location: {lat: 41.07321810000001,lng: 14.3270683}, id: 'ChIJMxPtKrJVOhMRRXKEJAoCnMc'},
-  {title:  'Bob & Jesael', location: {lat: 41.6457318,lng: 13.3477838}, id: 'ChIJ4Ydsg4VaJRMRV5Hm8O4TIcM'},
+  {title: 'La Passeggiata',
+  location: {lat: 41.5916799,lng: 13.2427548},
+  id: 'ChIJpR7JqQxDJRMRzk_oQbpBuLA'
+  },
+  {title: 'Ricordi Lievitati',
+  location: {lat: 41.590483,lng: 13.241865},
+  id: 'ChIJ1dX5PwtDJRMRYeOfHiynQ_k'
+  },
+  {title:  'Abbazia di Fossanova',
+  location: {lat: 41.4381536,lng: 13.1957528},
+  id: 'ChIJw8cFRMo_JRMRujicGH4ep0E'
+  },
+  {title:  'Villa d\'Este',
+  location: {lat: 41.9633123,lng: 12.7958058},
+  id: 'ChIJw-YlX3B4LxMRmeEzTuqQHr8'
+  },
+  {title:  'Reggia Caserta',
+  location: {lat: 41.07321810000001,lng: 14.3270683},
+  id: 'ChIJMxPtKrJVOhMRRXKEJAoCnMc'
+  },
+  {title:  'Bob & Jesael',
+  location: {lat: 41.6457318,lng: 13.3477838},
+  id: 'ChIJ4Ydsg4VaJRMRV5Hm8O4TIcM'
+  },
   // {title:  , location: {lat: ,lng:}, id: },
   // {title:  , location: {lat: ,lng:}, id: },
 ];
+
+//-------------ViewModel
+var ViewModel = function() {
+  var self = this;
+
+  this.placesList = ko.observableArray([]);
+  locations.forEach(function(location) {
+  });
+
+document.getElementById('go-filter').addEventListener('click', );
+
+function filterPlaces() {
+  var bounds = map.getBounds();
+  hideMarkers(placeMarkers);
+
+}
+
+function hideMarkers(markers) {
+  for (var i = 0; i < markers.length; i++) {
+    markers.[i].setMap(null);
+  }
+}
+
+}
+ko.applyBindings(new ViewModel());
+//--------------View
