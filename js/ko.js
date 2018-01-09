5dc0a4fb73c1923d9c9db35e936846a68cdf7976
Laura Logan <laurardh@aol.com>
2018-01-09 16:35:04 -0500

    corrects chaining to obtain postal code in foursquare results

--- js/ko.js
+++ js/ko.js
@@ -309,8 +309,7 @@ var NearByVenues = function(venue) {
     console.log("venue",venue);
   this.name = venue.venue.name ? venue.venue.name: "No name available";
   this.address = venue.venue.location.address ? venue.venue.location.address: "No address available";
-  this.zip = venue.venue.location.labeledLatLngs.postalCode ? venue.venue.location.labeledLatLngs.postalCode: "No postal code available";
-  console.log(venue.venue.location.labeledLatLngs.postalCode);
+  this.zip = venue.venue.location.postalCode ? venue.venue.location.postalCode: "No postal code available";
   this.city = venue.venue.location.city ? venue.venue.location.city: "No city available";
   this.country = venue.venue.location.country ? venue.venue.location.country: "No country available";
   this.url = venue.venue.url ?venue.venue.url: "No url available";
